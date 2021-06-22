7dfd27da1ae8f5d7653e8e0e353d64e4b1125eff
Andy <andywang3168@gmail.com>
2021-06-22 17:18:09 +0800

    完成 謝謝老師的指導

--- src/main/java/com/fju/Tester.java
+++ src/main/java/com/fju/Tester.java
@@ -4,48 +4,55 @@ package com.fju;
 import java.util.Scanner;
 
 public class Tester {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
 
-            Scanner s = new Scanner(System.in);
-            Drink[] drink = new Drink[6];
-            drink[0] = new Pocari();
-            drink[1] = new Supau();
-            drink[2] = new Grapejuice();
-            drink[3] = new Uniwater();
-            drink[4] = new Orangejuice();
-            drink[5] = new Pepsi();
-            int money = 0;
-            int a = 0;
-            System.out.println("===================================================================\n");
+        Scanner s = new Scanner(System.in);
+        Drink[] drink = new Drink[6];
+        drink[0] = new Pocari();
+        drink[1] = new Supau();
+        drink[2] = new Grapejuice();
+        drink[3] = new Uniwater();
+        drink[4] = new Orangejuice();
+        drink[5] = new Pepsi();
+
+        int inputmoney ;
+        int buyid ;
+        int buyprice = 0;
+        System.out.println("===================================================================\n");
 
 
-            System.out.println("items:1. POCARI SWEAT NT30 2. Super Supau NT30 3. Grape Juice NT15\n");
-            System.out.println("items:4. Uni Water NT15  5. Orange Juice NT12 6. Pepsi NT35\n");
-            System.out.println("===================================================================\n");
-            System.out.print("Don't put banknotes into vending machine! OK?\n");
-            System.out.print("Please enter how much did you put in:");
-            int n = s.nextInt();
+        System.out.println("items: 1. POCARI SWEAT NT30 2. Super Supau NT30  3. Grape Juice NT15\n");
+        System.out.println("items: 4. Uni Water NT15    5. Orange Juice NT12 6. Pepsi NT35\n");
+        System.out.println("===================================================================\n");
+        System.out.print("Don't put banknotes into vending machine! OK?\n");
+        System.out.print("Please enter how much did you put in:");
+        inputmoney = s.nextInt();
 
-            money = n;
+        if (inputmoney <= 0) {
+            System.out.println("please try again later\n");
+
+        } else {
+
             System.out.print("\nPlease enter the number which you want to buy : ");
-            int n1 = s.nextInt();
+            buyid = s.nextInt();
 
             for (Drink drinks : drink) {
-                if (drinks.id == n1) {
-                    a = drinks.price;
+                if (drinks.id == buyid) {
+                    buyprice = drinks.price;
                 }
             }
-            if ((n - a) >= 0) {
-                money -= a;
+            if ((inputmoney - buyprice) >= 0) {
+                inputmoney -= buyprice;
             } else {
                 System.out.println("your coins didn't enough");
             }
-            Coins c = new Coins();
-            c.setMoney(money);
-            c.getMoney();
+            Coins coins = new Coins();
+            coins.setMoney(inputmoney);
+            coins.getMoney();
 
 
+        }
     }
 }
 
